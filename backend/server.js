9de9ba41a825bb70eaaf2c53411601dc216013c7
Pupunfzx <beheraj542@gmail.com>
2026-02-09 13:54:06 +0530

    Fix vercel.json - remove catch-all rewrite

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
         const allowedOrigins = [
             'http://127.0.0.1:5501',
             'http://localhost:5501',
-            'https://robo-dashboard-kohl.vercel.app/'  
+            'https://robo-dashboard-kohl.vercel.app'  
         ];
         
         if (allowedOrigins.includes(origin)) {
