627286803980569a2530dbb575331659336f5b80
Pupunfzx <beheraj542@gmail.com>
2026-02-10 13:12:28 +0530

    fix hard refresh browser

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ app.use(cors({
         const allowedOrigins = [
             'http://127.0.0.1:5501',
             'http://localhost:5501',
-            'https://ionode.cloud'  
+            'https://ionode.cloud',
+            'https://www.ionode.cloud'  
         ];
         
         if (allowedOrigins.includes(origin)) {
